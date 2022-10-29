8971fe0e9fc4d9aec367ffc08c450efb0395e37a
blackbird14 <ntsrigaud14@gmail.com>
2022-10-29 15:22:52 +0800

    Web browser python

--- py_webB.py
+++ py_webB.py
@@ -4,11 +4,10 @@ import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
-cmd = "GET http://data.pr4e.org/romeo-full.txt HTTP/1.0\\n\\n".encode()
+cmd = "GET http://data.pr4e.org/romeo-full.txt HTTP/1.0\r\n\r\n".encode()
 mysock.send(cmd)
 
 while True:
-    print("LOOP ENTERED\n")
     data = mysock.recv(512)
     if (len(data) < 1):
         print("No data")
