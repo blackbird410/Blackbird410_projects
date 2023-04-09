d11c62acecab2213c798323076c4a0fe8de4c505
Neil Taison Rigaud <112826785+blackbird410@users.noreply.github.com>
2023-04-09 18:29:51 +0800

    Finalized project

--- budget.py
+++ budget.py
@@ -1,6 +1,3 @@
-#!\bin\env python
-
-
 class Category:
 
     def __init__(self, name = ""):
@@ -32,7 +29,6 @@ class Category:
             obj.deposit(amount, f"Transfer from {self.name}")
             return True
         else:
-            print("ERROR : Unable to transfer the amount.")
             return False
 
     def check_funds(self, amount):
@@ -113,21 +109,3 @@ def create_spend_chart(l_cat:[]):
         else:
             rs += " \n"
     return "Percentage spent by category\n" + rs[:-2] + " "
-
-
-#food = Category("Food")
-#food.deposit(1000, "initial deposit")
-#food.withdraw(10.15, "groceries")
-#food.withdraw(15.89, "restaurant and more food for dessert")
-#print(food.get_balance())
-#clothing = Category("Clothing")
-#food.transfer(50, clothing)
-#clothing.withdraw(25.55)
-#clothing.withdraw(100)
-#auto = Category("Auto")
-#auto.deposit(1000, "initial deposit")
-#auto.withdraw(15)
-
-#print(f"**********BAR_CHART**********\n{create_spend_chart([food, clothing, auto])}")
-
-
